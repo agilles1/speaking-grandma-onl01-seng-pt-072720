dc77f9cfe2e02ac111cfd1ee68a6d9117f6bd4fb
Adam Gillespie <adam.g.tpt@gmail.com>
2020-07-27 20:23:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
-  else puts "HUH?! speak up, sonny!"
+  else puts "HUH?! SPEAK UP, SONNY!"
   end
   binding.pry
 end
